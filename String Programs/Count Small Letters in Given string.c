55337d69b17477be24cee1b0b96febb701bcda75
Varsha Khot <72927737+varshakhot99@users.noreply.github.com>
2023-08-19 19:57:11 +0530

    Add files via upload

---
+++ String Programs/Count Small Letters in Given string.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char csrc[50]={'\0'};
+    int Ltr_cnt = 0 , i=0;
+
+    printf("\n\n\t*****************************************************************");
+
+    printf("\n\n\t\tEnter String = ");
+    gets(csrc);
+
+    while(csrc[i]!='\0')
+    {
+        if(csrc[i] >= 'a' && csrc[i] <= 'z')
+        {
+            Ltr_cnt++;
+        }
+        i++;
+    }
+
+    printf("\n\n\t\tCount of Capital Letters in given string is = %d",Ltr_cnt);
+
+    printf("\n\n\t*****************************************************************");
+
+
+    getch();
+    return 0;
+}
